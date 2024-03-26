a45db5a353bd0bbc2f8c1e02e00207fd96b1c106
LAPTOP-L2NIR9QD\Gutomo <gutomo999@gmail.com>
2024-03-26 21:04:56 +0700

    Update AppBar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,70 +1,74 @@
 import * as React from "react";
 import TopNavigation from "@cloudscape-design/components/top-navigation";
 
-//import { Auth } from 'aws-amplify';
-import { useAuthenticator } from '@aws-amplify/ui-react';
-//import {useEffect, useState} from "react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 export default () => {
+  const { user, signOut } = useAuthenticator((context) => [
+    context.user,
+    context.signOut,
+  ]);
 
-    //const [username, setUsername] = useState("");
-    const { user, signOut } = useAuthenticator((context) => [
-        context.user,
-        context.signOut
-      ]);
-    /*
-    useEffect(() => {
-       
-        const load = async () => {
-            Auth.currentUserInfo().then(
-                (result) => {
-                    setUsername(result.attributes.email);
-                }
-            )
-        };
+  function itemClickHandler(id) {
+    switch (id) {
+      case "profile":
+        console.log('profile');
+        break;
+      case "signout":
+        signOut();
+        break;        
+      default:
+        console.log('default');
+    }
+  }
 
-        load();
-    }, []);
-    */
-
-    return (
-        <TopNavigation
-            identity={{
-                href: "#",
-                title: "Online Document Manager",
-            }}
-            utilities={[
-                {
-                    type: "button",
-                    text: "Link",
-                    href: "https://aws.amazon.com",
-                    external: true,
-                    externalIconAriaLabel: " (opens in a new tab)"
-                },
-                {
-                    type: "button",
-                    text: "Sign out",
-                    iconName: "arrow-left",
-                    onClick: () => signOut()
-                },                
-                {
-                    type: "menu-dropdown",
-                    text: user.attributes.email,
-                    iconName: "user-profile",
-                    items: [
-                        {id: "profile", text: "Profile"},
-                        {id: "signout", text: "Sign out"},
-                    ]
-                }
-            ]}
-            i18nStrings={{
-                searchIconAriaLabel: "Search",
-                searchDismissIconAriaLabel: "Close search",
-                overflowMenuTriggerText: "More",
-                overflowMenuTitleText: "All",
-                overflowMenuBackIconAriaLabel: "Back",
-                overflowMenuDismissIconAriaLabel: "Close menu"
-            }}
-        />
-    );
-}
+  return (
+    <TopNavigation
+      identity={{
+        href: "#",
+        title: "Online Document Manager",
+      }}
+      utilities={[
+        {
+          type: "button",
+          text: "Link",
+          href: "https://aws.amazon.com",
+          external: true,
+          externalIconAriaLabel: " (opens in a new tab)",
+        },
+        {
+          type: "button",
+          iconName: "notification",
+          title: "Notifications",
+          ariaLabel: "Notifications (unread)",
+          badge: true,
+          disableUtilityCollapse: false
+        },        
+        {
+          type: "button",
+          text: "Sign out",
+          iconName: "arrow-left",
+          onClick: () => signOut(),
+        },
+        {
+          type: "menu-dropdown",
+          text: user.attributes.email,
+          iconName: "user-profile",
+          items: [
+            { id: "profile", text: "Profile" },
+            { id: "signout", text: "Sign out" },
+          ],
+          onItemClick: ({ detail }) => itemClickHandler(detail.id),
+        },
+      ]}
+      i18nStrings={{
+        searchIconAriaLabel: "Search",
+        searchDismissIconAriaLabel: "Close search",
+        overflowMenuTriggerText: "More",
+        overflowMenuTitleText: "All",
+        overflowMenuBackIconAriaLabel: "Back",
+        overflowMenuDismissIconAriaLabel: "Close menu",
+      }}
+    />
+  );
+};
